bb7b145eb4177e193e6fb919046b8ac4172c504a
Cesar Escalona <cesarescalona10@gmail.com>
2021-05-04 13:24:13 -0400

    Made some final changes displaying celsius

--- index.php
+++ index.php
@@ -16,10 +16,16 @@ session_start();
 $f3 = Base::instance();
 
 // Define default route
-$f3->route('GET /', function(){
+$f3->route('GET /', function($f3){
+    // save variables to the F3 'hive'
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('temp', '67');
+
     // Display the home page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/info.html');
 });
 
 // Run Fat-Free
